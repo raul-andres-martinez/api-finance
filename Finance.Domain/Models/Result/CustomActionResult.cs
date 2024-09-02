7114302564442d7065bd3c2652efbf9d12a24784
raul-andres-martinez <rams8@outlook.com>
2024-09-02 18:26:01 -0300

    refactor: Change result pattern

---
+++ Finance.Domain/Models/Result/CustomActionResult.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using Finance.Domain.Models.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PontoAPonto.Domain.Models
+{
+    public class CustomActionResult : IActionResult
+    {
+        public bool Success { get; }
+        public CustomError Error { get; }
+        public HttpStatusCode StatusCode { get; }
+
+        protected CustomActionResult(CustomError error)
+        {
+            Error = error ?? throw new ArgumentNullException(nameof(error));
+            Success = false;
+            StatusCode = error.StatusCode;
+        }
+
+        public CustomActionResult(HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            Success = true;
+            StatusCode = statusCode;
+            Error = new CustomError(HttpStatusCode.OK, "Success", "Success");
+        }
+
+        public virtual async Task ExecuteResultAsync(ActionContext context)
+        {
+            ObjectResult result;
+
+            if (Success)
+            {
+                result = new ObjectResult(null)
+                {
+                    StatusCode = (int?)StatusCode ?? 204
+                };
+            }
+            else
+            {
+                result = new ObjectResult(Error)
+                {
+                    StatusCode = (int?)Error?.StatusCode ?? 400
+                };
+            }
+
+            await result.ExecuteResultAsync(context);
+        }
+
+        public static CustomActionResult NoContent()
+        {
+            return new CustomActionResult(HttpStatusCode.NoContent);
+        }
+
+        public static implicit operator CustomActionResult(CustomError error)
+        {
+            return new CustomActionResult(error);
+        }
+    }
+
+    public class CustomActionResult<T> : CustomActionResult
+    {
+        public T Value { get; }
+
+        private CustomActionResult(T value, HttpStatusCode statusCode = HttpStatusCode.OK)
+            : base(statusCode)
+        {
+            Value = value;
+        }
+
+        private CustomActionResult(CustomError error)
+            : base(error)
+        {
+            Value = default!;
+        }
+
+        public static implicit operator CustomActionResult<T>(CustomError error)
+        {
+            return new CustomActionResult<T>(error);
+        }
+
+        public static implicit operator CustomActionResult<T>(T value)
+        {
+            return new CustomActionResult<T>(value);
+        }
+
+        public static implicit operator T(CustomActionResult<T> result)
+        {
+            return result.Value;
+        }
+
+        public async override Task ExecuteResultAsync(ActionContext context)
+        {
+            ObjectResult result;
+
+            if (Success)
+            {
+                result = new ObjectResult(Value)
+                {
+                    StatusCode = (int?)StatusCode ?? 200
+                };
+            }
+            else
+            {
+                result = new ObjectResult(Error)
+                {
+                    StatusCode = (int?)Error?.StatusCode ?? 400
+                };
+            }
+
+            await result.ExecuteResultAsync(context);
+        }
+    }
+}
---
+++ Finance.Domain/Models/Result/CustomError.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+
+namespace Finance.Domain.Models.Result
+{
+    public class CustomError
+    {
+        public CustomError(HttpStatusCode statusCode, string code, string message, object? value = default)
+        {
+            StatusCode = statusCode;
+            Code = code;
+            Message = message;
+            Value = value;
+        }
+
+        [JsonIgnore]
+        public HttpStatusCode StatusCode { get; }
+        public string Code { get; }
+        public string Message { get; }
+        public object? Value { get; }
+    }
+}
