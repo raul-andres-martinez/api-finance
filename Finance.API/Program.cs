d4762598a98045499b19ea9506aef1022249b901
raul-andres-martinez <rams8@outlook.com>
2024-09-08 01:44:47 -0300

    refactor: Refactor startup configs

--- Finance.API/Program.cs
+++ Finance.API/Program.cs
@@ -1,12 +1,5 @@
-using Finance.Domain.Models.Configs;
-using Finance.Persistence.Context;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Finance.Service.Extensions;
-using System.Text;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Finance.API.Middleware;
 
 namespace Finance.API
@@ -18,8 +11,36 @@ namespace Finance.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
+            ConfigureSwagger(builder);
+
+            builder.Services.AddServices(builder.Configuration);
+
+            var app = builder.Build();
+
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowSpecificOrigin");
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        private static void ConfigureSwagger(WebApplicationBuilder builder)
+        {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
@@ -50,70 +71,6 @@ namespace Finance.API
                     }
                 });
             });
-
-            builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection("JwtConfigs"));
-            builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<JwtConfiguration>>().Value);
-
-            builder.Services.Configure<EncryptionConfigs>(builder.Configuration.GetSection("EncryptionKey"));
-            builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<EncryptionConfigs>>().Value);
-
-            builder.Services.AddDbContext<FinanceContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            var configuration = builder.Configuration;
-
-            builder.Services.AddServices(configuration);
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowSpecificOrigin",
-                    policy =>
-                    {
-                        policy.WithOrigins("http://localhost:4200")
-                              .AllowAnyHeader()
-                              .AllowAnyMethod()
-                              .AllowCredentials();
-                    });
-            });
-
-            var jwtConfig = builder.Configuration.GetSection("JwtConfigs").Get<JwtConfiguration>() ?? throw new NullReferenceException();
-
-            builder.Services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.JwtKey ?? throw new NullReferenceException())),
-                    ValidateIssuer = true,
-                    ValidIssuer = jwtConfig.Issuer ?? throw new NullReferenceException(),
-                    ValidateAudience = false
-                };
-            });
-            builder.Services.AddAuthorization();
-
-            var app = builder.Build();
-
-            app.UseMiddleware<ExceptionHandlerMiddleware>();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("AllowSpecificOrigin");
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
         }
     }
 }
--- Finance.Service/Extensions/ServiceCollectionExtensions.cs
+++ Finance.Service/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,14 @@
 ﻿using Finance.Domain.Interfaces.Services;
+using Finance.Domain.Models.Configs;
+using Finance.Persistence.Context;
 using Finance.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Finance.Service.Extensions
 {
@@ -9,6 +16,18 @@ namespace Finance.Service.Extensions
     {
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
+            //Singleton instances
+            services.AddSingletonObjects(configuration);
+
+            //ConnString
+            services.ConfigureConnectionString(configuration);
+
+            //Jwt
+            services.ConfigureJwt(configuration);
+
+            //Cors
+            services.ConfigureCors();
+
             //Repositories
 
             //Services
@@ -18,6 +37,58 @@ namespace Finance.Service.Extensions
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
+        }
+
+        private static void AddSingletonObjects(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtConfiguration>(configuration.GetSection("JwtConfigs"));
+            services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<JwtConfiguration>>().Value);
+
+            services.Configure<EncryptionConfigs>(configuration.GetSection("EncryptionKey"));
+            services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<EncryptionConfigs>>().Value);
+        }
+
+        private static void ConfigureConnectionString(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FinanceContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        private static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtConfig = configuration.GetSection("JwtConfigs").Get<JwtConfiguration>() ?? throw new NullReferenceException();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.JwtKey ?? throw new NullReferenceException())),
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtConfig.Issuer ?? throw new NullReferenceException(),
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization();
+        }
+
+        private static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials();
+                    });
+            });
         }
     }
 }
